cb1ea1a10f9d025beebc4d48d179ab03454c4f5a
SashaKoro <balaskorotkov@gmail.com>
2017-01-29 11:26:28 -0500

    added projects page

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    welcomeMessage: 'Here live all my projects!!',
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send('Error handling the request!');
 });
